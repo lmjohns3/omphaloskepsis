af892cfb7e53415afd46480029ba2d848a803e6a
Leif Johnson <lmjohns3@gmail.com>
2023-06-28 08:50:49 -0700

    Update structure of creating new workout.

--- app/new-workout.jsx
+++ app/new-workout.jsx
@@ -1,52 +1,49 @@
 import React, { useEffect, useState } from 'react'
-import { useHistory, useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 
 import { apiCreate, apiRead } from './api.jsx'
+import lib from './lib.jsx'
 
 
 const NewWorkout = () => {
-  const history = useHistory()
+  const navigate = useNavigate()
   const [config, setConfig] = useState(null)
   const [goals, setGoals] = useState([])
-  const [defaults, setDefaults] = useState({})
-  const [showWorkouts, setShowWorkouts] = useState(true)
+  const [shuffle, setShuffle] = useState(false)
+  const [repeats, setRepeats] = useState(1)
   const [activeTag, activateTag] = useState(null)
 
   useEffect(() => { apiRead('config').then(setConfig) }, [])
 
   if (!config) return null
 
-  const addExercise = id => () => setGoals(cur => [ ...cur, { id: id, ...defaults } ])
+  const add = id => () => setGoals(cur => [ ...cur, { id } ])
 
-  const addFromWorkout = key => () => {
-    setGoals(cur => [
-      ...cur,
-      ...config.workouts[key].map(n => ({
-        id: config.exercises[config.nameToId[n]].id,
-        ...defaults,
-      }))
-    ])
-    setShowWorkouts(false)
-  }
-
-  const updateDefault = attr => value => {
-    if (value) setGoals(cur => cur.map(goal => ({ ...goal, [attr]: value })))
-    setDefaults(cur => {
-      if (value) cur[attr] = value
-      else delete cur[attr]
-      return { ...cur }
-    })
-  }
-
-  const updateExercise = (idx, attr) => value => setGoals(cur => [
-    ...cur.slice(0, idx),
-    { ...cur[idx], [attr]: value },
-    ...cur.slice(idx + 1)
+  const addFromWorkout = key => () => setGoals(cur => [
+    ...cur,
+    ...config.workouts[key].map(n => ({ id: config.nameToId[n] })),
   ])
 
-  const startWorkout = () => apiCreate(
-    'workouts', { tags: ['workout'], goals }
-  ).then(res => history.push(`/workout/${res.id.toString(36)}/`))
+  const update = idx => (attr, value) => setGoals(cur => [
+    ...cur.slice(0, idx),
+    { ...cur[idx], [attr]: value },
+    ...cur.slice(idx + 1),
+  ])
+
+  const updateAll = (attr, value) => {
+    console.log(attr, value)
+    setGoals(cur => cur.map(goal => ({ ...goal, [attr]: value })))
+  }
+
+  const startWorkout = () => {
+    let expanded = []
+    for (let i = 0; i < repeats; ++i)
+      expanded.push(...(shuffle ? lib.shuffle(goals) : goals))
+    apiCreate('workouts', { tags: ['workout'], goals: expanded })
+      .then(res => navigate(`/workout/${res.id.toString(36)}/`))
+  }
+
+  console.log(goals)
 
   return (
     <div className='workout new container'>
@@ -56,64 +53,63 @@ const NewWorkout = () => {
         {goals.length > 0 ? <button className='start' onClick={startWorkout}>⏱️ Start!</button> : null}
       </h1>
 
-      <table className='goals'>
-        <thead><tr><th>Exercise</th><th>Resistance</th><th>Reps</th><th>Duration</th><td></td></tr></thead>
-        <tbody>{goals.map((ex, i) => (
-          <tr key={`${ex.id}-${i}`}>
-            <td>{config.exercises[ex.id].name}</td>
-            <Cell key='resistance'
-                  value={ex.resistance}
-                  update={updateExercise(i, 'resistance')} />
-            <Cell key='reps'
-                  value={ex.reps}
-                  parse={parseInt}
-                  update={updateExercise(i, 'reps')} />
-            <Cell key='duration_s'
-                  value={ex.duration_s}
-                  format={value => value ? lib.formatDuration(value) : ''}
-                  parse={lib.parseDuration}
-                  update={updateExercise(i, 'duration_s')} />
-            <td><button className='remove'
-                        onClick={() => setGoals(cur => [
-                          ...cur.slice(0, i),
-                          ...cur.slice(i + 1),
-                        ])}>×</button></td>
-          </tr>))}
-          <tr className='defaults'>
-            <td>Defaults</td>
-            <Cell key='resistance'
-                  value={defaults.resistance}
-                  update={updateDefault('resistance')} />
-            <Cell key='reps'
-                  value={defaults.reps}
-                  parse={parseInt}
-                  update={updateDefault('reps')} />
-            <Cell key='duration_s'
-                  value={defaults.duration_s}
-                  format={lib.formatDuration}
-                  parse={lib.parseDuration}
-                  update={updateDefault('duration_s')} />
-          <td></td></tr>
-        </tbody>
-      </table>
+      {goals.length > 0 ? (
+        <table className='goals'>
+          <thead><tr><td></td><th>Exercise</th><th>🪨 Resistance</th><th>🧮 Amount</th><th>⏲️ Duration</th><td></td></tr></thead>
+          <tbody>{goals.map((goal, idx) => (
+            <tr key={idx}>
+              <td key='move'>{shuffle ? null : '☷'}</td>
+              <td key='name'>{config.exercises[goal.id].name}</td>
+              <Cell key='difficulty'
+                    attr='difficulty'
+                    goal={goal}
+                    updateAll={updateAll}
+                    update={update(idx)} />
+              <Cell key='amount'
+                    attr='amount'
+                    goal={goal}
+                    parse={parseInt}
+                    updateAll={updateAll}
+                    update={update(idx)} />
+              <Cell key='duration_s'
+                    attr='duration_s'
+                    goal={goal}
+                    format={value => value ? lib.formatDuration(value) : ''}
+                    parse={lib.parseDuration}
+                    updateAll={updateAll}
+                    update={update(idx)} />
+              <td key='remove'>
+                <button className='remove' onClick={() => setGoals(
+                          cur => [ ...cur.slice(0, idx), ...cur.slice(idx + 1) ])}>×</button>
+              </td>
+            </tr>))}
+          </tbody>
+        </table>
+      ) : null}
 
-      {showWorkouts ? (<>
-        <h2>Copy from another workout...</h2>
-        <ul className='other-workouts'>{Object.keys(config.workouts).map(key => (
-          config.workouts[key].length < 2 ? null : (
-            <li key={key}>
-              <button className='add' onClick={addFromWorkout(key)}>+</button>
-              <span className='name'>{key}</span>
-              {config.workouts[key].map(n => <span key={n} className='exercise'>{n}</span>)}
-            </li>
-          )))}</ul>
-      </>) : null}
+      {goals.length > 0 ? (<>
+        <label htmlFor='repeats'>Repeat</label>
+        <input id='repeats' value={repeats} onChange={e => setRepeats(e.target.value)} />
+        <label htmlFor='shuffle'>Shuffle</label>
+        <span id='shuffle' className={`toggle ${shuffle ? 'toggled' : ''}`}
+              onClick={() => setShuffle(s => !s)}></span>
+      </>): null}
+
+      <h2>Copy from another workout...</h2>
+      <ul className='other-workouts'>{Object.keys(config.workouts).map(key => (
+        config.workouts[key].length < 2 ? null : (
+          <li key={key}>
+            <button className='add' onClick={addFromWorkout(key)}>+</button>
+            <span className='name'>{key}</span>
+            {config.workouts[key].map(n => <span key={n} className='exercise'>{n}</span>)}
+          </li>
+        )))}</ul>
 
       <h2>Add individual exercises...</h2>
       <ul className='tags'>
         <li key='all'
             className={`tag ${activeTag ? '' : 'active'}`}
-            onClick={() => activateTag(null)}>All</li>
+            onClick={() => activateTag(null)}>all</li>
         {Object.keys(config.tagToIds).sort().map(tag => (
           <li key={tag}
               className={`tag ${tag === activeTag ? 'active' : ''}`}
@@ -121,33 +117,34 @@ const NewWorkout = () => {
         ))}
       </ul>
       <ul className='exercises'>{
-        Object
-          .values(config.exercises)
-          .sort((a, b) => a.name.localeCompare(b.name))
-          .map(ex => (
-            ex.tags.filter(t => t === activeTag).length + (activeTag === null) ? (
-              <li key={ex.id}>
-                <button className='add' onClick={addExercise(ex.id)}>+</button>
-                <span className='name'>{ex.name}</span>
-              </li>
-            ) : null))}</ul>
+        Object.values(config.exercises)
+              .sort((a, b) => a.name.localeCompare(b.name))
+              .map(ex => (activeTag === null || ex.tags.indexOf(activeTag) >= 0) ? (
+                <li key={ex.id}>
+                  <button className='add' onClick={add(ex.id)}>+</button>
+                  <img src={`/static/img/${ex.image}`} />
+                  <span className='name'>{ex.name}</span>
+                </li>
+              ) : null)
+      }</ul>
     </div>
   )
 }
 
 
-const Cell = ({ value, format, parse, update }) => {
+const Cell = ({ goal, attr, format, parse, update, updateAll }) => {
   const [isEditing, setIsEditing] = useState(false)
-  const f = format ? format : (v => v)
-  const p = parse ? parse : (v => v)
+  const fmt = format ? format : (v => v)
+  const prs = parse ? parse : (v => v)
   return <td>{isEditing ? (
     <input autoFocus
            onFocus={e => e.target.select()}
-           onBlur={e => { setIsEditing(false); update(p(e.target.value)) }}
-           defaultValue={f(value)} />
-  ) : (
-    <span onClick={() => setIsEditing(true)}>{value ? f(value) : '---'}</span>
-  )}</td>
+           onBlur={e => { setIsEditing(false); update(attr, prs(e.target.value)) }}
+           defaultValue={fmt(goal[attr])} />
+  ) : (<>
+    <span onClick={() => setIsEditing(true)}>{fmt(goal[attr]) || '---'}</span>
+    <button className='update-all' onClick={() => updateAll(attr, goal[attr])}>↕</button>
+  </>)}</td>
 }
 
 
