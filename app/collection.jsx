86ecafcfcb6d5726bdb3e55cb0e5051f88fb6435
Leif Johnson <lmjohns3@gmail.com>
2023-07-19 16:47:24 -0700

    Update collection view to use loader data.

--- app/collection.jsx
+++ app/collection.jsx
@@ -1,35 +1,17 @@
-import React, { useEffect, useState } from 'react'
-import { useNavigate, useParams } from 'react-router-dom'
-
-import { apiRead } from './api.jsx'
-import { Map } from './geo.jsx'
+import React from 'react'
+import { Link, useLoaderData } from 'react-router-dom'
 
 import './collection.styl'
 
 
 const Collection = () => {
-  const id = parseInt(useParams().id, 36)
-  const [collection, setCollection] = useState(null)
-
-  useEffect(() => { apiRead(`collection/${id}`).then(setCollection) }, [])
-
-  if (!collection) return null
+  const collection = useLoaderData()
 
   return (
-    <div className='collection'>
-      {collection.snapshots.map(snap => <Snapshot key={snap.id} snapshot={snap} />)}
-    </div>
-  )
-}
-
-
-const Snapshot = ({ snapshot }) => {
-  const navigate = useNavigate()
-
-  return (
-    <div className='snapshot' onClick={() => navigate(`/snapshot/${snapshot.id}/`)}>
-      {snapshot.utc}
-      {snapshot.note}
+    <div className='collection container'>
+      <ul>{collection.snapshot_ids.map(
+        id => <li><Link to={`/snapshot/${id}/`}>Snapshot</Link></li>
+      )}</ul>
     </div>
   )
 }
