6565b55c159cf4b168015c35a675a24756ba40d3
Leif Johnson <lmjohns3@gmail.com>
2023-07-19 16:50:42 -0700

    Rearrange routes with less complexity.

--- app/index.jsx
+++ app/index.jsx
@@ -6,13 +6,10 @@ import ReactDOM from 'react-dom/client'
 import {
   createBrowserRouter,
   Link,
-  Navigate,
   NavLink,
   Outlet,
   redirect,
   RouterProvider,
-  useLocation,
-  useNavigate,
   useRouteError,
 } from 'react-router-dom'
 
@@ -23,8 +20,7 @@ import { Account } from './account.jsx'
 import { AuthProvider, AuthRequired, useAuth } from './auth.jsx'
 import { Collection } from './collection.jsx'
 import { Mood } from './common.jsx'
-import { Map, useGeo } from './geo.jsx'
-import { Graphs } from './graphs.jsx'
+import { Habits } from './habits.jsx'
 import { Login } from './login.jsx'
 import { NewWorkout } from './new-workout.jsx'
 import { Snapshot } from './snapshot.jsx'
@@ -43,41 +39,18 @@ const Splash = () => (
 )
 
 
-const Dashboard = () => {
-  const navigate = useNavigate()
-  const [mood, setMood] = useState(null)
-  const [note, setNote] = useState('')
-  const [coords, setCoords] = useState(null)
-
-  useGeo().then(geo => setCoords(geo?.coords))
-
-  return (
-    <div className='dashboard container'>
-      <h1>Welcome.</h1>
-      <div className='cards'>
-        <div className='card'>
-          <h2>Where are you?</h2>
-        </div>
-        <div className='card'>
-          <h2>How are you feeling?</h2>
-          <Mood value={mood} update={setMood} />
-        </div>
-        <div className='card'>
-          <h2>Want to write?</h2>
-          <textarea autoFocus defaultValue={note} onChange={e => setNote(e.target.value)}></textarea>
-        </div>
-      </div>
-      <button onClick={() => apiCreate('snapshots', { mood, note }).then(res => navigate(`/snapshot/${res.id}/`))}>Save</button>
-    </div>
-  )
-}
+const Dashboard = () => <div className='dashboard container'><h1>Welcome.</h1></div>
 
 
-const Error = err => {
-  const location = useLocation()
-  return location.pathname.startsWith('/login') ?
-    <Login /> : <Navigate to='/login/' replace state={{ then: location }} />
-}
+const Error = err => (
+  <div className='error container'>
+    <h1>Error</h1>
+    <pre>{JSON.stringify(err, 4)}</pre>
+  </div>
+)
+
+
+const Index = () => useAuth().token ? <Dashboard /> : <Splash />
 
 
 const App = () => {
@@ -87,12 +60,19 @@ const App = () => {
 
   const nav = token ? (
     <ul>
-      <li><NavLink title='Dashboard' to='/'>🖥️️</NavLink></li>
-      <li><NavLink title='Timeline' to='/timeline/'>🗓️</NavLink></li>
-      <li><NavLink title='Graphs' to='/graphs/'>📊️</NavLink></li>
+      <li><NavLink title='Dashboard' to='/'>👁️️</NavLink></li>
+      <li><NavLink title='Timeline' to='/timeline/'>📅️</NavLink></li>
+      <li><NavLink title='Habits' to='/habits/'>☑️</NavLink></li>
       <li className='sep'></li>
-      <li><Link title='New Snapshot' to='/snapshot/new/'>📷️</Link>️</li>
-      <li><Link title='Start Sleeping' to='/sleep/new/'>💤</Link></li>
+      <li><Link title='New Snapshot' onClick={e => {
+                  e.preventDefault()
+                  apiCreate('snapshots').then(res => redirect(`/snapshot/${res.id}/`))
+                }}>📷️</Link>️</li>
+      <li><Link title='Start Sleeping' onClick={e => {
+                  e.preventDefault()
+                  apiCreate('snapshots', { flavor: 'sleep' })
+                    .then(res => redirect(`/snapshot/${res.id}/`))
+                }}>💤</Link></li>
       <li><Link title='New Workout' to='/workout/new/'>🏋️</Link></li>
       <li className='sep'></li>
       <li><NavLink title='Settings' to='/account/'>⚙️</NavLink></li>
@@ -112,49 +92,50 @@ ReactDOM.createRoot(
       path: '/',
       loader: () => apiUpdate('token').catch(() => null),
       element: <AuthProvider><App /></AuthProvider>,
+      //errorElement: <Error />,
       children: [
-        { path: '/login/', element: <Login /> },
-        { path: '/splash/', element: <StrictMode><Splash /></StrictMode> },
         {
-          path: '/',
-          element: <AuthRequired><Outlet /></AuthRequired>,
-          errorElement: <Error />,
-          children: [
-            { index: true, element: <Dashboard /> },
-            { path: '/account/', loader: () => apiRead('account'), element: <Account /> },
-            { path: '/timeline/', loader: () => apiRead('timeline'), element: <Timeline /> },
-            { path: '/graphs/', loader: () => apiRead('graphs'), element: <Graphs /> },
-            { path: '/workout/new/', loader: () => apiRead('workouts'), element: <NewWorkout /> },
-            {
-              path: '/sleep/new/',
-              loader: () => apiCreate(
-                'snapshots', { flavor: 'sleep' }
-              ).then(res => redirect(`/snapshot/${res.id}/`)),
-              element: <div>Loading...</div>,
-            },
-            {
-              path: '/snapshot/new/',
-              loader: () => apiCreate(
-                'snapshots'
-              ).then(res => redirect(`/snapshot/${res.id}/`)),
-              element: <div>Loading...</div>,
-            },
-            {
-              path: '/snapshot/:id/',
-              loader: ({ params }) => apiRead(`snapshot/${params.id}`),
-              element: <Snapshot />,
-            },
-            {
-              path: '/collection/:id/',
-              loader: ({ params }) => apiRead(`collection/${params.id}`),
-              element: <Collection />,
-            },
-            {
-              path: '/workout/:id/',
-              loader: ({ params }) => apiRead(`collection/${params.id}`),
-              element: <Workout />,
-            },
-          ],
+          index: true,
+          element: <Index />,
+        },
+        {
+          path: '/account/',
+          loader: () => apiRead('account'),
+          element: <AuthRequired><Account /></AuthRequired>,
+        },
+        {
+          path: '/collection/:id/',
+          loader: ({ params }) => apiRead(`collection/${params.id}`),
+          element: <AuthRequired><Collection /></AuthRequired>,
+        },
+        {
+          path: '/habits/',
+          loader: () => apiRead('habits'),
+          element: <AuthRequired><Habits /></AuthRequired>,
+        },
+        {
+          path: '/login/',
+          element: <Login />,
+        },
+        {
+          path: '/snapshot/:id/',
+          loader: ({ params }) => apiRead(`snapshot/${params.id}`),
+          element: <AuthRequired><Snapshot /></AuthRequired>,
+        },
+        {
+          path: '/timeline/',
+          loader: () => apiRead('timeline'),
+          element: <AuthRequired><Timeline /></AuthRequired>,
+        },
+        {
+          path: '/workout/new/',
+          loader: () => apiRead('workouts'),
+          element: <AuthRequired><NewWorkout /></AuthRequired>,
+        },
+        {
+          path: '/workout/:id/',
+          loader: ({ params }) => apiRead(`collection/${params.id}`),
+          element: <AuthRequired><Workout /></AuthRequired>,
         },
       ],
     }])} />
